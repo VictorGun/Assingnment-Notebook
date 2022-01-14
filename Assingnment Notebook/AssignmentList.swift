c844374734b301a3d6a248782f84d7e9872e204d
Victor <victor>
2022-01-14 11:05:55 -0600

    Half Way Done

---
+++ Assingnment Notebook/AssignmentList.swift
@@ -0,0 +1,8 @@
+//
+//  AssignmentList.swift
+//  Assingnment Notebook
+//
+//  Created by Student on 1/14/22.
+//
+
+import Foundation
--- Assingnment Notebook/ContentView.swift
+++ Assingnment Notebook/ContentView.swift
@@ -8,9 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var assignmentItems = [AssignmentItem(course: "Math", description: "Complete Worksheet 17-1", dueDate: Date()), AssignmentItem(course: "Science", description: "Finish Page 6-7", dueDate: Date()), AssignmentItem(course: "History", description: "Read Chapter 10", dueDate: Date())]
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                ForEach(assignmentItems) {item in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(item.course)
+                                .font(.headline)
+                            Text(item.description)
+                        }
+                        Spacer()
+                        Text(item.dueDate, style: .date)
+                    }
+                }
+                .onMove(perform: { indices, newOffset in
+                    assignmentItems.move(fromOffsets: indices, toOffset: newOffset)
+                })
+                .onDelete(perform: { indexSet in
+                    assignmentItems.remove(atOffsets: indexSet)
+                })
+            }
+            .navigationBarTitle("Courses", displayMode: .inline)
+            .navigationBarItems(leading: EditButton())
+        }
     }
 }
 
@@ -18,4 +40,10 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+struct AssignmentItem: Identifiable {
+    var id = UUID()
+    var course = String()
+    var description = String()
+    var dueDate = Date()
 }
