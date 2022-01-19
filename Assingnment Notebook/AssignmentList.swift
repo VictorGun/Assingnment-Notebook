52f09317d7be0158a6872fcb6aea01c446b6747c
Victor <victor>
2022-01-19 10:24:50 -0600

    Enabled data persistence

--- Assingnment Notebook/AssignmentList.swift
+++ Assingnment Notebook/AssignmentList.swift
@@ -8,7 +8,23 @@
 import Foundation
 
 class AssignmentList: ObservableObject {
-    @Published var items = [AssignmentItem(course: "Math", description: "Do page 35", dueDate: Date()),
-                            AssignmentItem(course: "Science", description: "Finish Lab Questions", dueDate: Date()),
-                            AssignmentItem(course: "History", description: "Read Chapter 5", dueDate: Date())]
- }
+    @Published var items : [AssignmentItem] {
+        didSet {
+            let encoder = JSONEncoder()
+            if let encoded = try? encoder.encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "data")
+            }
+        }
+    }
+    init() {
+        if let items = UserDefaults.standard.data(forKey: "data") {
+            let decoder = JSONDecoder()
+            if let decoded = try? decoder.decode([AssignmentItem].self, from: items) {
+                self.items = decoded
+                return
+            }
+        }
+        items = []
+    }
+}
+
